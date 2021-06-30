6e59934f7cc67371442fcd570cb676298dbc0b84
ThaiHa1510 <32334725+ThaiHa1510@users.noreply.github.com>
2021-07-01 00:18:09 +0700

    create manager local storage

---
+++ JsonStorage.js
@@ -0,0 +1,20 @@
+export class JSONStorage {
+  constructor(storage) {
+    if (!storage || typeof storage !== "object")
+      throw new Error(`Expected a Storage object, got ${storage}`)
+    this.storage = storage
+  }
+  set(k, v) {
+    const str = JSON.stringify(v)
+    this.storage.setItem(k, v)
+  }
+  get(k) {
+    const str = this.storage.getItem(k)
+    // we return `undefined` instead of `null`, so that we can differentiate between the cases when the key doesn't exist vs when the value is a JSON-serialized `"null"`
+    if (str === null) return
+    return JSON.parse(str)
+  }
+  remove(k) {
+    this.storage.removeItem(k)
+  }
+}
